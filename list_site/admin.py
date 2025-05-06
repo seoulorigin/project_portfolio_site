1e1b394944c4d711dcfc09f509a99d18e8dafcdb
김경원 <seoulorigin@seoulorigin-MacBookAir.local>
2025-05-06 18:08:59 +0900

    admin 페이지 | 총 점수 오름차순

--- list_site/admin.py
+++ list_site/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
-from .models import Project
-# Register your models here.
-admin.site.register(Project)
+from .models import Project, Score
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'total_score')
+    ordering = ('-total_score',)
+    readonly_fields = ('total_score',)
+    
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ('id', 'project', 'score')
+    list_filter = ('project',)
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Score, ScoreAdmin)
--- list_site/models.py
+++ list_site/models.py
@@ -5,7 +5,22 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length=50)
     desc = models.TextField(default = "기본 설명")
+    total_score = models.IntegerField(default=0)
+
+    def update_total_score(self):
+        self.total_score = sum(score.score for score in self.score_set.all())
+        self.save()
+        
+    total_score.short_description = "총점"
     
 class Score(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     score = models.IntegerField()
+    
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        self.project.update_total_score()
+
+    def delete(self, *args, **kwargs):
+        super().delete(*args, **kwargs)
+        self.project.update_total_score()
